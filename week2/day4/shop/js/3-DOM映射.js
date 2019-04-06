93c12cca6e9a3bc6d1a6c96b95c4c1f27906618e
mabin <1164664451@qq.com>
2019-04-06 13:25:14 +0800

    商品排序上午

--- week2/day4/shop/js/3-DOM映射.js
+++ week2/day4/shop/js/3-DOM映射.js
@@ -6,6 +6,9 @@ let headerBox = document.getElementById('header'),
   listBox = document.getElementById('list'),
   productList = listBox.getElementsByTagName('li');
 
+  // 使用querySelectorAll获取
+  // productList = document.querySelectorAll('#list>li');
+  console.log('数据绑定前 =>', productList);
 // 1. 基于ajax获取数据
 let xhr = new XMLHttpRequest(); // 创建一个ajax实例对象
 xhr.open('GET', 'json/product.json', false); // 调用xhr的open方法
@@ -43,6 +46,9 @@ for (let i = 0; i < productData.length; i++) {
 
 // 把拼接好的html字符串插入到页面的指定容器中（这些拼接好的字符串在没有添加到页面中，再此之前还不是元素）
 listBox.innerHTML = str;
+// console.log('数据绑定后 =>', productList);
+
+console.log('productList => ', productList);
 
 // 基于价格给productList里面的li排序
 
@@ -77,6 +83,20 @@ linkList[1].onclick = function () {
   sortList();
 };
 
+/*
+* DOM映射：页面中的html元素和我们通过js相关方法（getElementsByTagName/getElementById/getElementsByxxxx）获取元素集合或者是对象存在映射关系。（一个改了另一个跟着改）
+*
+* list[i].style.backgroundColor = 'red': 将list[i]元素对象对应的堆内存空间下的style属性下的backgroundColor属性修改为red（本质是操作js的堆内存空间），但是由于DOM映射机制，页面中的元素和元素对象的堆内存地址是绑定在一起。所以我们修改了元素对象堆内存空间里面的值，页面中的标签会按照最新的值来渲染；
+*
+* 数据绑定之前，我们获取ul下面的li得到一个空集合，但是再数据绑定后，我们不需要重新获取，DOM映射机制会把新增的元素映射到我们之前获取的集合中，此时元素集合不在是空集合。但是querySelectorAll/querySelector获取的集合叫做 静态集合 【staticNodeList】，而且是掐断了DOM映射的，基于这种方式获取的元素集合，在数据绑定完成后需要重新获取。
+*
+* productList 是ul下面的li集合，集合中存储的是元素对象的堆内存地址；
+* {0: xxxffff000, 1: xxxfff111, 2: xxxfff222, ....,length: 10}。后来我们把productList转换成数组，在这个过程中只是把每个li元素对象的堆内存地址复制到了数组中 [xxxfff000, xxxfff111, xxxfff222...]。然后我们appendChild时就是在把appendChild(xxxfff000【堆内存地址】)，此时appendChild发现xxxfff000这个堆内存空间在页面中有对应的li，所以它不会克隆一个新的，而是把那个对应的li移动到容器的末尾。
+*
+* appendChild: 是向容器的末尾追加元素，如果追加的元素已经存在于之前的容器中了，此时并不会克隆（按照原来的，再造一份一模一样的新的）一份再追加，而是把原来的移动到容器的末尾。
+*
+*
+* */
 
 
 
